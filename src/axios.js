b1084c254043d112db1103d285f8b5edc855e09d
emputed <amejoradan1600@alumno.ipn.mx>
2024-11-24 22:03:54 -0600

    Conexion a render

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://express-nutrec.onrender.com/api/v1',
-  baseURL: 'http://localhost:4000/api/v1/',
+  baseURL: 'https://express-nutrec.onrender.com/api/v1',
+  //baseURL: 'http://localhost:4000/api/v1/',
 });
 
 export default api;
