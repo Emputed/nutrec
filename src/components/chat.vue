1cf7cb1db84d98786f66731d3f008d8ed7c24de5
emputed <amejoradan1600@alumno.ipn.mx>
2024-11-28 22:24:42 -0600

    Se quita el polling de la conexion

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -53,9 +53,7 @@ export default {
     const idStore = useIdStore();
     let user = ref([]);
     console.log(user);
-    const socket = io("https://express-nutrec.onrender.com", {
-      transports:["websocket"]
-    });
+    const socket = io("https://express-nutrec.onrender.com");
     const messages = ref([]);
     const newMessage = ref("");
     const paciente = ref({});
